37a78222c586c553dcc5861cde4849c6b47867cf
Curtis Fulton <Curtis33210@hotmail.com>
2016-08-22 00:50:09 +1000

    Added untracked Creature files

---
+++ Assets/Scripts/Objects/Creature/Creature.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Creature 
+{
+	public int Health { get; private set; }
+    public Tile CurrentTile { get; private set; }
+
+    public Creature() {
+
+    }
+
+    public void MoveTo(Tile newTile) {
+
+    }
+}
